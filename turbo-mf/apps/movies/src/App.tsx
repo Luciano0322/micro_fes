99aff1d058e6cae0e2eb0d2336feeba160e0d577
Luciano Lee <luciano.lee@webcomm.com.tw>
2022-06-21 10:08:44 +0000

    在package/ui內處理共用組件

--- turbo-mf/apps/movies/src/App.tsx
+++ turbo-mf/apps/movies/src/App.tsx
@@ -1,9 +1,31 @@
-import { Button } from "ui";
+import { AppShell } from 'ui';
 
 function App() {
   return (
-    <div className="App">
-      <Button/>
+    <div>
+      <AppShell 
+        title="Movies" 
+        routes={[
+          {
+            path: "/",
+            element: () => <div>This is Home Page</div>,
+          },
+          {
+            path: "/playlist",
+            element: () => <div>This is Playlist Page</div>,
+          },
+        ]}
+        navLinks={[
+          {
+            label: "Home",
+            path: "/"
+          },
+          {
+            label: "Playlist",
+            path: "/playlist"
+          }
+        ]}
+      />
     </div>
   );
 }
